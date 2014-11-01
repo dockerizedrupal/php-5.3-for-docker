2f2b18a603fe331c3bffd27626e3df25bbb73463
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-01 21:51:54 +0200

    Update

---
+++ src/build/modules/php/manifests/fpm.pp
@@ -0,0 +1,33 @@
+class php::fpm {
+  require php::packages
+  require php::phpfarm
+  require php::cgi
+
+  file { '/phpfarm/src/php-5.3.29.tar.gz':
+    ensure => present,
+    source => 'puppet:///modules/php/phpfarm/src/php-5.3.29.tar.gz'
+  }
+
+  exec { 'tar xzf php-5.3.29.tar.gz':
+    cwd => '/phpfarm/src',
+    path => ['/bin'],
+    require => File['/phpfarm/src/php-5.3.29.tar.gz']
+  }
+
+  file { '/phpfarm/src/custom/options-5.3.29.sh':
+    ensure => present,
+    source => 'puppet:///modules/php/phpfarm/src/custom/options-5.3.29-fpm.sh',
+    mode => 755,
+    require => Exec['tar xzf php-5.3.29.tar.gz']
+  }
+
+  exec { '/phpfarm/src/main.sh 5.3.29':
+    timeout => 0,
+    require => File['/phpfarm/src/custom/options-5.3.29.sh']
+  }
+
+  exec { 'rm -rf /phpfarm/src/php-5.3.29':
+    path => ['/bin'],
+    require => Exec['/phpfarm/src/main.sh 5.3.29']
+  }
+}
--- src/build/modules/php/manifests/init.pp
+++ src/build/modules/php/manifests/init.pp
@@ -3,33 +3,8 @@ class php {
   require php::phpfarm
   require php::supervisor
 
-  file { '/phpfarm/src/php-5.3.29.tar.gz':
-    ensure => present,
-    source => 'puppet:///modules/php/phpfarm/src/php-5.3.29.tar.gz'
-  }
-
-  exec { 'tar xzf php-5.3.29.tar.gz':
-    cwd => '/phpfarm/src',
-    path => ['/bin'],
-    require => File['/phpfarm/src/php-5.3.29.tar.gz']
-  }
-
-  exec { 'Compile PHP (CGI)':
-    command => '/bin/bash -c "cp /src/build/modules/php/files/phpfarm/src/custom/options-5.3.29-cgi.sh /phpfarm/src/custom/options-5.3.29.sh && /phpfarm/src/main.sh 5.3.29"',
-    timeout => 0,
-    require => Exec['tar xzf php-5.3.29.tar.gz']
-  }
-
-  exec { 'Compile PHP (FPM)':
-    command => '/bin/bash -c "cp /src/build/modules/php/files/phpfarm/src/custom/options-5.3.29-fpm.sh /phpfarm/src/custom/options-5.3.29.sh && /phpfarm/src/main.sh 5.3.29"',
-    timeout => 0,
-    require => Exec['Compile PHP (CGI)']
-  }
-
-  exec { 'rm -rf /phpfarm/src/php-5.3.29':
-    path => ['/bin'],
-    require => Exec['Compile PHP (FPM)']
-  }
+  recuire php::cgi
+  recuire php::fpm
 
   file { '/phpfarm/inst/php-5.3.29/etc/php-fpm.conf':
     ensure => present,

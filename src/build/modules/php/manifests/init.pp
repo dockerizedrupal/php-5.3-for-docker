3d346443cb1b7eb5465e91557ee07353ede6ee70
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-27 14:40:05 +0200

    Update

--- src/build/modules/php/manifests/init.pp
+++ src/build/modules/php/manifests/init.pp
@@ -14,7 +14,7 @@ class php {
     require => File['/tmp/php-5.3.29.tar.gz']
   }
 
-  exec { 'mv php-5.3.29 /phpfarm/src/php-5.3.29':
+  exec { 'cp -r php-5.3.29 /phpfarm/src/php-5.3.29':
     cwd => '/tmp',
     path => ['/bin'],
     require => Exec['tar xzf php-5.3.29.tar.gz']
@@ -24,7 +24,7 @@ class php {
     ensure => present,
     source => 'puppet:///modules/php/phpfarm/src/custom/options-5.3.29.sh',
     mode => 755,
-    require => Exec['mv php-5.3.29 /phpfarm/src/php-5.3.29']
+    require => Exec['cp -r php-5.3.29 /phpfarm/src/php-5.3.29']
   }
 
   exec { '/phpfarm/src/main.sh 5.3.29':
@@ -32,9 +32,26 @@ class php {
     require => File['/phpfarm/src/custom/options-5.3.29.sh']
   }
 
+  exec { 'rm -r /phpfarm/src/php-5.3.29':
+    path => ['/bin'],
+    require => File['/phpfarm/src/main.sh 5.3.29']
+  }
+
+  exec { 'cp -r php-5.3.29 /phpfarm/src/php-5.3.29':
+    cwd => '/tmp',
+    path => ['/bin'],
+    require => Exec['rm -r /phpfarm/src/php-5.3.29']
+  }
+
+  exec { 'cp /src/build/modules/php/files/phpfarm/src/custom/options-5.3.29-fpm.sh /phpfarm/src/custom/options-5.3.29.sh':
+    cwd => '/tmp',
+    path => ['/bin'],
+    require => Exec['cp -r php-5.3.29 /phpfarm/src/php-5.3.29']
+  }
+
   exec { '/bin/bash -c "PHPFPM=1 && /phpfarm/src/main.sh 5.3.29"':
     timeout => 0,
-    require => Exec['/phpfarm/src/main.sh 5.3.29']
+    require => Exec['cp /src/build/modules/php/files/phpfarm/src/custom/options-5.3.29-fpm.sh /phpfarm/src/custom/options-5.3.29.sh']
   }
 
   exec { 'rm -rf /phpfarm/src/php-5.3.29':

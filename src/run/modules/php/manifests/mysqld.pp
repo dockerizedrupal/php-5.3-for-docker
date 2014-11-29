4c47b83f8bca69f42b27167583b018dff4abbe8a
Jürgen Viljaste <j.viljaste@gmail.com>
2014-11-29 20:22:04 +0200

    Update

--- src/run/modules/php/manifests/mysqld.pp
+++ src/run/modules/php/manifests/mysqld.pp
@@ -1,4 +1,8 @@
 class php::mysqld {
+  exec { 'mkdir -p /var/run/mysqld':
+    path => ['/bin']
+  }
+
   file { '/etc/supervisor/conf.d/mysqld.conf':
     ensure => present,
     content => template('php/mysqld.conf.erb'),

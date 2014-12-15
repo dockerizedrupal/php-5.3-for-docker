df974557ef2ba8a4c43620de20804dcafbe522e5
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-15 19:56:56 +0200

    Update

--- src/build/modules/php/manifests/drush/packages.pp
+++ src/build/modules/php/manifests/drush/packages.pp
@@ -4,4 +4,10 @@ class php::drush::packages {
   exec { '/bin/su - root -c "curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename composer"':
     timeout => 0
   }
+
+  package {[
+      'mysql-client'
+    ]:
+    ensure => present
+  }
 }

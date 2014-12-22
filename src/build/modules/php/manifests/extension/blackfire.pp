3ec31fc22f834221cc3463742d16aac489da64bf
Jürgen Viljaste <j.viljaste@gmail.com>
2014-12-23 00:36:14 +0200

    Update

--- src/build/modules/php/manifests/extension/blackfire.pp
+++ src/build/modules/php/manifests/extension/blackfire.pp
@@ -5,14 +5,9 @@ class php::extension::blackfire {
     path => ['/bin']
   }
 
-  exec { 'mkdir -p /phpfarm/inst/php-5.3.29/lib/php/extensions/no-debug-non-zts-20090626':
-    path => ['/bin']
-  }
-
   file { '/phpfarm/inst/php-5.3.29/lib/php/extensions/no-debug-non-zts-20090626/blackfire-php-linux_amd64-php-53.so':
     ensure => present,
-    source => 'puppet:///modules/php/tmp/blackfire-php-linux_amd64-php-53.so',
-    require => Exec['mkdir -p /phpfarm/inst/php-5.3.29/lib/php/extensions/no-debug-non-zts-20090626']
+    source => 'puppet:///modules/php/tmp/blackfire-php-linux_amd64-php-53.so'
   }
 
   exec { '/bin/su - root -c "curl -s https://packagecloud.io/gpg.key | apt-key add -"': }
--- src/build/modules/php/manifests/extension/zend_debugger.pp
+++ src/build/modules/php/manifests/extension/zend_debugger.pp
@@ -1,13 +1,8 @@
 class php::extension::zend_debugger {
   require php
 
-  exec { 'mkdir -p /phpfarm/inst/php-5.3.29/lib/php/extensions/no-debug-non-zts-20090626':
-    path => ['/bin']
-  }
-
   file { '/phpfarm/inst/php-5.3.29/lib/php/extensions/no-debug-non-zts-20090626/ZendDebugger.so':
     ensure => present,
-    source => 'puppet:///modules/php/tmp/ZendDebugger.so',
-    require => Exec['mkdir -p /phpfarm/inst/php-5.3.29/lib/php/extensions/no-debug-non-zts-20090626']
+    source => 'puppet:///modules/php/tmp/ZendDebugger.so'
   }
 }

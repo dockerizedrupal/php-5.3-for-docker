a38a76731886c92dc75cdb38243457a928662c9e
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-12 19:00:06 +0200

    Update

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-apt-get update
-
 puppet apply --modulepath=/src/build/modules /src/build/build.pp
-
-/src/php.sh clean
--- src/php.sh
+++ src/php.sh
@@ -2,12 +2,9 @@
 
 case "${1}" in
   build)
-    /bin/su - root -mc /src/build.sh
-    ;;
-  clean)
-    /bin/su - root -mc /src/clean.sh
+    /bin/su - root -mc "apt-get update && /src/build.sh && /src/clean.sh"
     ;;
   run)
-    /bin/su - root -mc /src/run.sh
+    /bin/su - root -mc "source /src/variables.sh && /src/run.sh"
     ;;
 esac

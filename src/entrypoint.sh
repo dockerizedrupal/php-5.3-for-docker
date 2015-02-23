232f0a97c221676bc12b9a79f12563c26487c9b1
Jürgen Viljaste <j.viljaste@gmail.com>
2015-02-23 19:09:19 +0200

    Update

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,9 +2,9 @@
 
 case "${1}" in
   build)
-    /bin/su - root -mc "apt-get update && /src/build.sh && /src/clean.sh"
+    /bin/su - root -mc "apt-get update && /src/php/build.sh && /src/php/clean.sh"
     ;;
   run)
-    /bin/su - root -mc "source /src/variables.sh && /src/run.sh"
+    /bin/su - root -mc "source /src/php/variables.sh && /src/php/run.sh"
     ;;
 esac

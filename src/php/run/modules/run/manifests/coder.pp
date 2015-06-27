fef0185b8b0c9d1325591f493b334273a653c47a
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-27 20:29:19 +0300

    Update

--- src/php/run/modules/run/manifests/coder.pp
+++ src/php/run/modules/run/manifests/coder.pp
@@ -1,9 +1,14 @@
 class run::coder {
+  require run::user
+
   if $drupal_version == '6' {
     file { '/home/container/.drush/coder':
       ensure => link,
       target => '/usr/local/src/coder6'
     }
+
+    bash_exec { "chown -R -h $user_id /usr/local/src/coder6": }
+    bash_exec { "chgrp -R -h $group_id /usr/local/src/coder6": }
   }
   elsif $drupal_version == '7' {
     file { '/home/container/.drush/coder':
@@ -15,5 +20,11 @@ class run::coder {
       ensure => link,
       target => '/usr/local/src/coder7/coder_sniffer/Drupal'
     }
+
+    bash_exec { "chown -R -h $user_id /usr/local/src/coder7": }
+    bash_exec { "chgrp -R -h $group_id /usr/local/src/coder7": }
+
+    bash_exec { "chown -R -h $user_id /usr/local/src/PHP_CodeSniffer_1": }
+    bash_exec { "chgrp -R -h $group_id /usr/local/src/PHP_CodeSniffer_1": }
   }
 }
--- src/php/run/modules/run/manifests/drush.pp
+++ src/php/run/modules/run/manifests/drush.pp
@@ -1,4 +1,6 @@
 class run::drush {
+  require run::user
+
   if $drupal_version == '6' {
     file { '/usr/local/bin/drush':
       ensure => link,
@@ -9,6 +11,9 @@ class run::drush {
       ensure => link,
       target => '/usr/local/src/drush6/drush.complete.sh'
     }
+
+    bash_exec { "chown -R -h $user_id /usr/local/src/drush6": }
+    bash_exec { "chgrp -R -h $group_id /usr/local/src/drush6": }
   }
 
   if $drupal_version == '7' {
@@ -21,5 +26,8 @@ class run::drush {
       ensure => link,
       target => '/usr/local/src/drush7/drush.complete.sh'
     }
+
+    bash_exec { "chown -R -h $user_id /usr/local/src/drush7": }
+    bash_exec { "chgrp -R -h $group_id /usr/local/src/drush7": }
   }
 }

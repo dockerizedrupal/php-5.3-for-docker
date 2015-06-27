9409c3b4cfdff1846ab9dd12c90432f52922194b
Jürgen Viljaste <j.viljaste@gmail.com>
2015-06-27 11:03:14 +0300

    Update

--- src/php/run/modules/run/manifests/coder.pp
+++ src/php/run/modules/run/manifests/coder.pp
@@ -1,6 +1,6 @@
 class run::coder {
   if $drupal_version == '6' {
-    file { '/root/.drush/coder':
+    file { '/home/container/.drush/coder':
       ensure => link,
       target => '/usr/local/src/coder6'
     }

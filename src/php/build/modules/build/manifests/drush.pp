5255a48758c973e0a0d448a168a3d422336cd8ba
Jürgen Viljaste <j.viljaste@gmail.com>
2015-07-01 23:52:52 +0300

    Update

--- src/php/build/modules/build/manifests/drush.pp
+++ src/php/build/modules/build/manifests/drush.pp
@@ -7,22 +7,22 @@ class build::drush {
 
   bash_exec { "su - container -c 'mkdir /home/container/.drush'": }
 
-  file { '/tmp/drush-6.5.0.tar.gz':
+  file { '/tmp/drush-6.6.0.tar.gz':
     ensure => present,
-    source => 'puppet:///modules/build/tmp/drush-6.5.0.tar.gz'
+    source => 'puppet:///modules/build/tmp/drush-6.6.0.tar.gz'
   }
 
-  bash_exec { 'cd /tmp && tar xzf drush-6.5.0.tar.gz':
-    require => File['/tmp/drush-6.5.0.tar.gz']
+  bash_exec { 'cd /tmp && tar xzf drush-6.6.0.tar.gz':
+    require => File['/tmp/drush-6.6.0.tar.gz']
   }
 
-  bash_exec { 'mv /tmp/drush-6.5.0 /usr/local/src/drush6':
-    require => Bash_exec['cd /tmp && tar xzf drush-6.5.0.tar.gz']
+  bash_exec { 'mv /tmp/drush-6.6.0 /usr/local/src/drush6':
+    require => Bash_exec['cd /tmp && tar xzf drush-6.6.0.tar.gz']
   }
 
   bash_exec { 'cd /usr/local/src/drush6 && composer install':
     timeout => 0,
-    require => Bash_exec['mv /tmp/drush-6.5.0 /usr/local/src/drush6']
+    require => Bash_exec['mv /tmp/drush-6.6.0 /usr/local/src/drush6']
   }
 
   file { '/tmp/drush-7.0.0.tar.gz':

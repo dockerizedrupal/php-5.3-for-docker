b8b0d2e7045f54433de7dbf7447a9e85b44333a6
Jürgen Viljaste <j.viljaste@gmail.com>
2015-09-02 08:20:03 +0300

    Update

--- src/php-5.3/build/modules/build/manifests/php53.pp
+++ src/php-5.3/build/modules/build/manifests/php53.pp
@@ -45,13 +45,13 @@ class build::php53 {
     require => Bash_exec['rm -r /usr/local/src/phpfarm/src/php-5.3.29']
   }
 
-  bash_exec { 'cp /src/-5.3/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.3.29-fpm.sh /usr/local/src/phpfarm/src/custom/options-5.3.29.sh':
+  bash_exec { 'cp /src/php-5.3/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.3.29-fpm.sh /usr/local/src/phpfarm/src/custom/options-5.3.29.sh':
     require => Bash_exec['CP=1 && cp -r /tmp/php-5.3.29 /usr/local/src/phpfarm/src/php-5.3.29']
   }
 
   bash_exec { 'PHPFPM=1 && /usr/local/src/phpfarm/src/main.sh 5.3.29':
     timeout => 0,
-    require => Bash_exec['cp /src/-5.3/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.3.29-fpm.sh /usr/local/src/phpfarm/src/custom/options-5.3.29.sh']
+    require => Bash_exec['cp /src/php-5.3/build/modules/build/files/usr/local/src/phpfarm/src/custom/options-5.3.29-fpm.sh /usr/local/src/phpfarm/src/custom/options-5.3.29.sh']
   }
 
   bash_exec { 'rm -rf /usr/local/src/phpfarm/src/php-5.3.29':

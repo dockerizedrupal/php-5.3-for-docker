6449c10cdcf1cafa51d4664cdd872ca8de21cd03
Jürgen Viljaste <j.viljaste@gmail.com>
2015-12-20 02:00:27 +0200

    #17

--- src/php-5.3/build/modules/build/manifests/php53/supervisor.pp
+++ src/php-5.3/build/modules/build/manifests/php53/supervisor.pp
@@ -1,7 +1,7 @@
 class build::php53::supervisor {
-  file { '/etc/supervisor/conf.d/php-5.3.conf':
+  file { '/etc/supervisor/conf.d/php.conf':
     ensure => present,
-    source => 'puppet:///modules/build/etc/supervisor/conf.d/php-5.3.conf',
+    source => 'puppet:///modules/build/etc/supervisor/conf.d/php.conf',
     mode => 644
   }
 }

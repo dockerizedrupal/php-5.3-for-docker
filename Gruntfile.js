cb3c91a9c07de6126a54e5de26976160a9854a89
Jürgen Viljaste <j.viljaste@gmail.com>
2016-09-03 00:32:40 +0300

    #37

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
-var current_version = '1.2.11';
-var new_version = '1.2.12';
+var current_version = '1.2.12';
+var new_version = '1.2.13';
 
 module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-replace');
--- Vagrantfile
+++ Vagrantfile
@@ -1,6 +1,4 @@
-VERSION = "1.2.12"
-
-require 'yaml'
+VERSION = "1.2.13"
 
 required_plugins = %w(vagrant-vbguest)
 
